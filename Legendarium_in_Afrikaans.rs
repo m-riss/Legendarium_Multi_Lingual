eeebde22692441a0f15e6d2920c17a218d161c8b
m-riss <95452643+m-riss@users.noreply.github.com>
2023-11-04 20:36:31 -0400

    Create Legendarium_in_Afrikaans.rs

---
+++ Legendarium_in_Afrikaans.rs
@@ -0,0 +1,32 @@
+use std::io;
+
+fn main() {
+let mut Exit = String::new();
+
+println!("J. R. R. Tolkien - Die Here van die ringe. (1/4)");
+println!("-----------------------------------------------");
+println!("DIE HERE VAN DIE RINGE");
+println!("deur");
+println!("J. R. R. TOLKIEN");
+println!("Deel een: The Fellowship of the Ring");
+println!("Deel Twee: Die Twee Torings");
+println!("Deel drie: Die terugkeer van die koning");
+println!("Voltooi met indeks en volledige bylaes");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+println!("Gaan uit?");
+io::stdin().read_line(&mut Exit);
+}
