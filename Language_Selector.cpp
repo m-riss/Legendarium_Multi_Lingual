3432363312d99402d78cebfa220660f114e88384
m-riss <95452643+m-riss@users.noreply.github.com>
2023-11-04 20:01:28 -0400

    Create Language_Selector.cpp

---
+++ Language_Selector.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+int Exit_Variable = 0;
+
+
+string Language_Selection = "";
+
+
+string QuestionMark = "";
+
+
+QuestionMark = "?";
+
+cout << "What Language Would You Like To Read Tolkien's Legendarium In" << QuestionMark << endl;
+cin >> Language_Selection;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cout << "Exit" << QuestionMark << endl;
+cin >> Exit_Variable;
+return 0;
+}
