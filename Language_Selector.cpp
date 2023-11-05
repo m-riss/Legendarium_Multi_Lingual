d8ff893149f2d9c09237fc4218e1b9212a599527
m-riss <95452643+m-riss@users.noreply.github.com>
2023-11-04 20:37:06 -0400

    Update Language_Selector.cpp

--- Language_Selector.cpp
+++ Language_Selector.cpp
@@ -9,21 +9,37 @@ using std::string;
 int main() {
 int Exit_Variable = 0;
 
+//Defining every language in Google Translate as a string variable.
+string Afrikaans = "";
+string afrikaans = "";
+string Albanian = "";
+string albanian = "";
+
+
+
 
 string Language_Selection = "";
-
-
 string QuestionMark = "";
+string Colon = "";
 
+Afrikaans = "Afrikaans";
+afrikaans = "afrikaans";
 
 QuestionMark = "?";
+Colon = ":";
 
 cout << "What Language Would You Like To Read Tolkien's Legendarium In" << QuestionMark << endl;
 cin >> Language_Selection;
 
+if (Language_Selection == Afrikaans) {
+cout << "You Can Download the Afrikaans Version of Tolkien's Legendarium At This Link" << Colon << endl;
+cout << "https://tinyurl.com/bddnx59t" << endl;
+}
 
-
-
+if (Language_Selection == afrikaans) {
+cout << "You Can Download the Afrikaans Version of Tolkien's Legendarium At This Link" << Colon << endl;
+cout << "https://tinyurl.com/bddnx59t" << endl;
+}
 
 
 
