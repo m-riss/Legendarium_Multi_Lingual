3c85e49bed3fdf93b3be13cce9b7aee12c7f61b1
m-riss <95452643+m-riss@users.noreply.github.com>
2023-11-04 21:29:56 -0400

    Update Legendarium_in_Afrikaans.rs

--- Legendarium_in_Afrikaans.rs
+++ Legendarium_in_Afrikaans.rs
@@ -12,12 +12,61 @@ println!("Deel een: The Fellowship of the Ring");
 println!("Deel Twee: Die Twee Torings");
 println!("Deel drie: Die terugkeer van die koning");
 println!("Voltooi met indeks en volledige bylaes");
-
-
-
-
-
-
+println!("Drie ringe vir die Elfkonings onder die lug,");
+println!("Sewe vir die Dwerg-here in hulle klipsale,");
+println!("Nege vir sterflike mans wat gedoem is om te sterf,");
+println!("Een vir die Donker Heer op sy donker troon,");
+println!("In die land van Mordor waar die skaduwees lê.");
+println!("Een ring om hulle almal te regeer,");
+println!("Een ring om hulle te vind,");
+println!("Een Ring om hulle almal te bring en in die duisternis bind hulle,");
+println!("In die land van Mordor waar die skaduwees lê.");
+println!("INHOUD");
+println!("VOORWOORD");
+println!("PROLOGUE");
+println!("1. Betreffende Hobbits");
+println!("2. Met betrekking tot pyp-onkruid");
+println!("3. Van die Orde van die Shire");
+println!("4. Van die Bevinding van die Ring");
+println!("Nota op die Shire-rekords");
+println!("DIE GEMEENSKAP VAN DIE RING");
+println!("Boek Een:");
+println!("Hoofstuk Een 'n Langverwagte Partytjie");
+println!("Hoofstuk Twee Die skaduwee van die verlede");
+println!("Hoofstuk Drie Drie is Maatskappy");
+println!("Hoofstuk Vier 'n Kortpad na sampioene");
+println!("Hoofstuk Vyf 'n Sameswering ontmasker");
+println!("Hoofstuk Ses Die Ou Bos");
+println!("Hoofstuk sewe in die huis van Tom Bombadil");
+println!("Hoofstuk Agt Mis op die Barrow-Downs");
+println!("Hoofstuk Nege by die teken van die steigerende ponie");
+println!("Hoofstuk Tien Strider");
+println!("Hoofstuk elf 'n Mes in die donker");
+println!("Hoofstuk Twaalf Vlug na die Ford");
+println!("Boek Twee:");
+println!("Hoofstuk Een Baie Vergaderings");
+println!("Hoofstuk Twee Die Raad van Elrond");
+println!("Hoofstuk Drie Die Ring Gaan Suid");
+println!("Hoofstuk Vier 'n Reis in die donker");
+println!("Hoofstuk Vyf Die Brug van Khazad-dûm");
+println!("Hoofstuk Ses Lothlórien");
+println!("Hoofstuk Sewe Die spieël van Galadriel");
+println!("Hoofstuk Agt Vaarwel aan Lórien");
+println!("Hoofstuk Nege Die Groot Rivier");
+println!("Hoofstuk Tien Die Verbreking van die Genootskap");
+println!("DIE TWEE TORINGS");
+println!("Boek Drie:");
+println!("Hoofstuk Een Die vertrek van Boromir");
+println!("Hoofstuk Twee Die ruiters van Rohan");
+println!("Hoofstuk Drie Die Uruk-Hai");
+println!("Hoofstuk Vier Boombaard");
+println!("Hoofstuk Vyf Die Wit Ruiter");
+println!("Hoofstuk Ses Die Koning van die Goue Saal");
+println!("Hoofstuk Sewe Roer is diep");
+println!("Hoofstuk Agt Die pad na Isengard");
+println!("Hoofstuk Nege Flotsam en Jetsam");
+println!("Hoofstuk Tien Die Stem van Saruman");
+println!("Hoofstuk Elf Die Palantir");
 
 
 
