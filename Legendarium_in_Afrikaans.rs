15ffe7aba60a233058c19e12fa67c0c1c5097b9b
m-riss <95452643+m-riss@users.noreply.github.com>
2023-11-09 19:20:32 -0500

    Create Legendarium_in_Afrikaans.rs

---
+++ Legendarium_in_Afrikaans.rs
@@ -0,0 +1,41 @@
+use std::io;
+fn main() {
+let mut exit = String::new();
+let _exit_input = String::new();
+
+println!("J. R. R. Tolkien - Die Here van die ringe.");
+println!("(Een van vier)");
+println!("-----------------------------------------------");
+println!("DIE HERE VAN DIE RINGE");
+println!("deur");
+println!("J. R. R. TOLKIEN");
+
+println!("Deel een: Die Genootskap van die Ring");
+println!("Deel Twee: Die Twee Torings");
+println!("Deel Drie: Die terugkeer van die koning");
+println!("Voltooi met indeks en volledige bylaes");
+println!("Drie ringe vir die elwe-konings onder die lug,");
+println!("Sewe vir die Dwerg-here in hul klipsale,");
+println!("Nege vir sterflike mans wat gedoem is om te sterf,");
+println!("Een vir die Donker Heer op sy donker troon,");
+println!("In die land van Mordor waar die skaduwees lê.");
+println!("Een ring om hulle almal te regeer,");
+println!("Een ring om hulle te vind,");
+println!("Een Ring om hulle almal te bring en hulle in die duisternis te bind,");
+println!("In die land van Mordor waar die skaduwees lê.");
+println!("INHOUD");
+println!("VOORWOORD");
+println!("VOORWOORD");
+
+
+
+
+
+
+
+
+
+
+println!("Gaan uit?");
+let _exit_input = io::stdin().read_line(&mut exit);
+}
